cf0e80376f2ef66ca0e5febd1042f2aacffe5363
sejuntgbest <brais@naver.com>
2018-07-09 14:13:27 +0900

    더치페이 계산기 추가 20180709

--- prunusEsjay/src/format/HelloWorld.java
+++ prunusEsjay/src/format/HelloWorld.java
@@ -1,30 +1,20 @@
 package format;
 import java.math.BigDecimal;
 import format.myFormat;
-
+import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		/**
 		 * @param args
-		 */
-		System.out.println("The beginning of the Program!");
-		/*
-		System.out.println("Hellow World");
-		System.out.println("1           ");
-		System.out.println("2");
-		System.out.println("3");*/
-		BigDecimal mbr_mlg = new BigDecimal(12222);
-		BigDecimal multiply_ten = new BigDecimal(10);
-		//mbr_mlg = 3;	//현재회원 마일리지 갖고옴
-		mbr_mlg = mbr_mlg.divide(multiply_ten, 0, BigDecimal.ROUND_DOWN);
-		mbr_mlg = mbr_mlg.multiply(multiply_ten);
-		System.out.println("주문시 마일리지 사용금액 보여줄때, [" + mbr_mlg + " 원]");
-		
+		 * 헬로월드 시작 3일차
+		 */		
 		myFormat CHECKS = new myFormat();
-		
-		String test = CHECKS.Test();
-		
-		System.out.println("테스트 나옴 ?"+test);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요.");
+		System.out.println("1: 더치페이 계산기 , 2: 3: 4: 5:");
+		int code = sc.nextInt();
+		String stringValue = CHECKS.stringFormatTest(code);		
+		System.out.println("결과입니다. \n "+stringValue);
 	}
 }
--- prunusEsjay/src/format/myFormat.java
+++ prunusEsjay/src/format/myFormat.java
@@ -1,9 +1,45 @@
 package format;
-import format.myFormat;
-public class myFormat {
+import javax.xml.transform.Result;
 
-		public String Test(){
-			String aaa = "1111";
-			return aaa;
+import format.myFormat;
+import java.util.Scanner;
+
+public class myFormat {
+	/* 테스트 20180705  */
+	public String stringFormatTest(int intValue){
+		String returnString = "";
+		Scanner sc = new Scanner(System.in);
+		
+		switch(intValue) {
+			case 1 :
+				returnString = "1번 선택하셨습니다.";
+				System.out.println("더치페이) 총 금액을 입력 해 주세요.");
+				float amount = sc.nextFloat();
+				System.out.println("더치페이) 몇 명인가요?");
+				int ppl = sc.nextInt();
+				float douch = douchPayValue(amount,ppl);
+				System.out.println("1명당 "+douch + "원 결제 하시면 됩니다.");
+				break;
+			case 2 :
+				returnString = "2번 선택하셨습니다.";
+				break;
+			case 3 :
+				returnString = "3번 선택하셨습니다.";
+				break;
+			case 4 :
+				returnString = "4번 선택하셨습니다.";
+				break;
+			default :
+				returnString = "기본값 선택하셨습니다.";
+				break;
 		}
+		return returnString;
+	}
+	
+	/*
+	 *  더치페이하기.
+	 */
+	public float douchPayValue(float amount, int ppl) {
+		return  amount / (float) ppl;
+	}
 }

781dd34d74436fd2a00f544f91e3ec09f8186b67
sejuntgbest <brais@naver.com>
2018-07-16 17:48:04 +0900

    SEJUNGBEST CALENDAR FORMAT 20180716 VER.0002
    CALENDAR FORMAT MODIFICATION FROM ANYTHING!

--- prunusEsjay/src/format/myFormat.java
+++ prunusEsjay/src/format/myFormat.java
@@ -29,8 +29,20 @@ public class myFormat {
 			case 2 :
 				System.out.println("시간구하기) 현재 시각을 가져옵니다. \n");
 				System.out.println("현재 시각.. "+ cal.getToday());
-				System.out.println("시스템의 밀리초 구하기. "+ cal.getMiliSeconds()+"\n");
-				System.out.println("경과시간(초) 구하기. "+ cal.getAfterMiliSeconds(cal.getMiliSeconds())+"\n");
+				System.out.println("시스템의 밀리초 구하기. 									"+ cal.getMiliSeconds()+"\n");
+				System.out.println("경과시간(초) 구하기. 									"+ cal.getAfterMiliSeconds(cal.getMiliSeconds())+"\n");
+				System.out.println("Date를 Calendar로 맵핑하기								"+"\n");
+				cal.returnCalendar();
+				System.out.println("날짜(년/월/일/시/분/초) 구하기								"+ cal.simpleFormatDate()+"\n");
+				System.out.println("날짜(년/월/일/시/분/초) 구하기	2							");
+				System.out.println(">>>>>>>												");	cal.simpleGregorianFormatDate();
+				System.out.println(">>>>>>>												");
+				System.out.println("날짜(년/월/일/시/분/초) 구하기	3							"+ cal.getDateInstance()+"\n");
+				System.out.println("표준시간대를 지정하고 날짜를 가져오기							"+ cal.getTimeZone("JST")+"\n");
+				System.out.println("영어로된 날짜를 숫자로 바꾸기								"+ cal.getStdDateToFormattedDate(cal.getToday())+"\n");
+				System.out.println("\"Sun, 5 Dec 1999 00:07:21\"를 \"1999-12-05\"로 바꾸기   "+ cal.getStdDateToFormattedDate2(cal.getToday())+"\n");
+				System.out.println("숫자 12자리를 다시 날짜로 변환하기						    "+ cal.getNumberToDate()+"\n");
+				
 				returnString = "2번 선택하셨습니다. \n";
 				break;
 			case 3 :
